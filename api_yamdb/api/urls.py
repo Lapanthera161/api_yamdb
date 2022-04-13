87ca420908cc25f15a5e7ec42a1dc8c07c43d45d
Catherina Govorova <ei.govorova@yandex.ru>
2022-04-13 14:13:40 +0300

    second v.1

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet)
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
